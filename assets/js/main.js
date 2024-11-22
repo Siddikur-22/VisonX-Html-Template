07c9ede4779513d4e691a523af1457609f5e6115
Siddikur-22 <designsiddik@gmail.com>
2024-11-22 17:07:15 +0600

    home two responsive complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,34 @@
 	// 	time: 1500
 	// });
 
+// ====================
+	// Progrees Bar
+	// ====================
+	var skillPers = document.querySelectorAll(".experience-bar-per");
+
+	skillPers.forEach(function(skillPer) {
+	var per = parseFloat(skillPer.getAttribute("data-per"));
+	skillPer.style.width = per + "%";
+	
+	var animatedValue = 0; 
+	var startTime = null;
+	
+	function animate(timestamp) {
+		if (!startTime) startTime = timestamp;
+		var progress = timestamp - startTime;
+		var stepPercentage = progress / 1000; // Dividing by duration in milliseconds (1000ms = 1s)
+		
+		if (stepPercentage < 1) {
+		animatedValue = per * stepPercentage;
+		skillPer.setAttribute("data-per", Math.floor(animatedValue) + "%");
+		requestAnimationFrame(animate);
+		} else {
+		animatedValue = per;
+		skillPer.setAttribute("data-per", Math.floor(animatedValue) + "%");
+		}
+	}
+	requestAnimationFrame(animate);
+	}); 
 
 
   // logo area marquee start
