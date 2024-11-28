7d2f2f7cca42d29663ac1dc29381a9cb9ac09a77
Siddikur-22 <designsiddik@gmail.com>
2024-11-28 12:26:04 +0600

    js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,22 @@
 
 (function ($) {
   "use strict";
+	// Preloader 
+	jQuery(window).on('load', function () {
+		$(".preloader").delay(1600).fadeOut("slow");
+	});
+
+	$('.sidebar-button').on("click", function () {
+        $(this).toggleClass('active');
+    });
+
+	document.querySelector('.sidebar-button').addEventListener('click', () =>
+		document.querySelector('.main-menu').classList.toggle('show-menu'));
+
+	$('.menu-close-btn').on("click", function () {
+		$('.main-menu').removeClass('show-menu');
+	});
+
 
   const element = document.querySelectorAll(".badge__char");
   const step = 360 / element.length;
@@ -54,421 +70,25 @@
   });
 
   // ---------------
+  // service image 
+  const serviceImgItem = document.querySelectorAll(".services-wrap .single-services ");
 
-  if ($("body").not(".is-mobile").hasClass("tt-magic-cursor")) {
-    if ($(window).width() > 1024) {
-      gsap.config({
-        nullTargetWarn: false,
-        trialWarn: false,
-      });
-      $(".magnetic-item").wrap('<div class="magnetic-wrap"></div>');
+	function __followImageCursor(event, serviceImgItem) {
+	  const contentBox = serviceImgItem.getBoundingClientRect();
+	  const dx = event.clientX - contentBox.x;
+	  const dy = event.clientY - contentBox.y;
+	  serviceImgItem.children[2].style.transform = `translate(${dx}px, ${dy}px) rotate(12deg)`;
+	}
+  
+	serviceImgItem.forEach((item, i) => {
+	  item.addEventListener("mousemove", (event) => {
+		setInterval(__followImageCursor(event, item), 100);
+	  });
+	});
 
-      if ($("a.magnetic-item").length) {
-        $("a.magnetic-item").addClass("not-hide-cursor");
-      }
 
-      var $mouse = { x: 0, y: 0 }; // Cursor position
-      var $pos = { x: 0, y: 0 }; // Cursor position
-      var $ratio = 0.15; // delay follow cursor
-      var $active = false;
-      var $ball = $("#ball");
 
-      var $ballWidth = 20; // Ball default width
-      var $ballHeight = 20; // Ball default height
-      var $ballOpacity = 0.5; // Ball default opacity
-      var $ballBorderWidth = 2; // Ball default border width
 
-      gsap.set($ball, {
-        // scale from middle and style ball
-        xPercent: -50,
-        yPercent: -50,
-        width: $ballWidth,
-        height: $ballHeight,
-        borderWidth: $ballBorderWidth,
-        opacity: $ballOpacity,
-      });
-
-      document.addEventListener("mousemove", mouseMove);
-
-      function mouseMove(e) {
-        $mouse.x = e.clientX;
-        $mouse.y = e.clientY;
-      }
-
-      gsap.ticker.add(updatePosition);
-
-      function updatePosition() {
-        if (!$active) {
-          $pos.x += ($mouse.x - $pos.x) * $ratio;
-          $pos.y += ($mouse.y - $pos.y) * $ratio;
-
-          gsap.set($ball, { x: $pos.x, y: $pos.y });
-        }
-      }
-
-      $(".magnetic-wrap").mousemove(function (e) {
-        parallaxCursor(e, this, 2); // magnetic ball = low number is more attractive
-        callParallax(e, this);
-      });
-
-      function callParallax(e, parent) {
-        parallaxIt(e, parent, parent.querySelector(".magnetic-item"), 25); // magnetic area = higher number is more attractive
-      }
-
-      function parallaxIt(e, parent, target, movement) {
-        var boundingRect = parent.getBoundingClientRect();
-        var relX = e.clientX - boundingRect.left;
-        var relY = e.clientY - boundingRect.top;
-
-        gsap.to(target, {
-          duration: 0.3,
-          x: ((relX - boundingRect.width / 2) / boundingRect.width) * movement,
-          y:
-            ((relY - boundingRect.height / 2) / boundingRect.height) * movement,
-          ease: Power2.easeOut,
-        });
-      }
-
-      function parallaxCursor(e, parent, movement) {
-        var rect = parent.getBoundingClientRect();
-        var relX = e.clientX - rect.left;
-        var relY = e.clientY - rect.top;
-        $pos.x =
-          rect.left + rect.width / 2 + (relX - rect.width / 2) / movement;
-        $pos.y =
-          rect.top + rect.height / 2 + (relY - rect.height / 2) / movement;
-        gsap.to($ball, { duration: 0.3, x: $pos.x, y: $pos.y });
-      }
-
-      // Magic cursor behavior
-      // ======================
-
-      // Magnetic item hover.
-      $(".magnetic-wrap")
-        .on("mouseenter mouseover", function (e) {
-          $ball.addClass("magnetic-active");
-          gsap.to($ball, { duration: 0.3, width: 70, height: 70, opacity: 1 });
-          $active = true;
-        })
-        .on("mouseleave", function (e) {
-          $ball.removeClass("magnetic-active");
-          gsap.to($ball, {
-            duration: 0.3,
-            width: $ballWidth,
-            height: $ballHeight,
-            opacity: $ballOpacity,
-          });
-          gsap.to(this.querySelector(".magnetic-item"), {
-            duration: 0.3,
-            x: 0,
-            y: 0,
-            clearProps: "all",
-          });
-          $active = false;
-        });
-
-      // Alternative cursor style on hover.
-      $(
-        ".cursor-alter, .tt-main-menu-list > li > a, .tt-main-menu-list > li > .tt-submenu-trigger > a"
-      )
-        .not(".magnetic-item") // omit from selection.
-        .on("mouseenter", function () {
-          gsap.to($ball, {
-            duration: 0.3,
-            borderWidth: 0,
-            opacity: 0.2,
-            backgroundColor: "#CCC",
-            width: "90px",
-            height: "90px",
-          });
-        })
-        .on("mouseleave", function () {
-          gsap.to($ball, {
-            duration: 0.3,
-            borderWidth: $ballBorderWidth,
-            opacity: $ballOpacity,
-            backgroundColor: "transparent",
-            width: $ballWidth,
-            height: $ballHeight,
-            clearProps: "backgroundColor",
-          });
-        });
-
-      // Cursor view on hover (data attribute "data-cursor="...").
-      $("[data-cursor]").each(function () {
-        $(this)
-          .on("mouseenter", function () {
-            $ball
-              .addClass("ball-view")
-              .append('<div class="ball-view-inner"></div>');
-            $(".ball-view-inner").append($(this).attr("data-cursor"));
-            gsap.to($ball, {
-              duration: 0.3,
-              yPercent: -75,
-              width: 82,
-              height: 82,
-              opacity: 1,
-              borderWidth: 0,
-            });
-            gsap.to(".ball-view-inner", {
-              duration: 0.3,
-              scale: 1,
-              autoAlpha: 1,
-            });
-          })
-          .on("mouseleave", function () {
-            gsap.to($ball, {
-              duration: 0.3,
-              yPercent: -50,
-              width: $ballWidth,
-              height: $ballHeight,
-              opacity: $ballOpacity,
-              borderWidth: $ballBorderWidth,
-            });
-            $ball.removeClass("ball-view").find(".ball-view-inner").remove();
-          });
-        $(this).addClass("not-hide-cursor");
-      });
-
-      // Cursor drag on hover (class "cursor-drag"). For Swiper sliders.
-      $(".swiper").each(function () {
-        if ($(this).parent().attr("data-simulate-touch") === "true") {
-          if ($(this).parent().hasClass("cursor-drag")) {
-            $(this)
-              .on("mouseenter", function () {
-                $ball.append('<div class="ball-drag"></div>');
-                gsap.to($ball, {
-                  duration: 0.3,
-                  width: 60,
-                  height: 60,
-                  opacity: 1,
-                });
-              })
-              .on("mouseleave", function () {
-                $ball.find(".ball-drag").remove();
-                gsap.to($ball, {
-                  duration: 0.3,
-                  width: $ballWidth,
-                  height: $ballHeight,
-                  opacity: $ballOpacity,
-                });
-              });
-            $(this).addClass("not-hide-cursor");
-
-            // Ignore "data-cursor" on hover.
-            $(this)
-              .find("[data-cursor]")
-              .on("mouseenter mouseover", function () {
-                $ball.find(".ball-drag").remove();
-                return false;
-              })
-              .on("mouseleave", function () {
-                $ball.append('<div class="ball-drag"></div>');
-                gsap.to($ball, {
-                  duration: 0.3,
-                  width: 60,
-                  height: 60,
-                  opacity: 1,
-                });
-              });
-          }
-        }
-      });
-
-      // Cursor drag on mouse down / click and hold effect (class "cursor-drag-mouse-down"). For Swiper sliders.
-      $(".swiper").each(function () {
-        if ($(this).parent().attr("data-simulate-touch") === "true") {
-          if ($(this).parent().hasClass("cursor-drag-mouse-down")) {
-            $(this)
-              .on("mousedown pointerdown", function (e) {
-                if (e.which === 1) {
-                  // Affects the left mouse button only!
-                  gsap.to($ball, {
-                    duration: 0.2,
-                    width: 60,
-                    height: 60,
-                    opacity: 1,
-                  });
-                  $ball.append('<div class="ball-drag"></div>');
-                }
-              })
-              .on("mouseup pointerup", function () {
-                $ball.find(".ball-drag").remove();
-                if ($(this).find("[data-cursor]:hover").length) {
-                } else {
-                  gsap.to($ball, {
-                    duration: 0.2,
-                    width: $ballWidth,
-                    height: $ballHeight,
-                    opacity: $ballOpacity,
-                  });
-                }
-              })
-              .on("mouseleave", function () {
-                $ball.find(".ball-drag").remove();
-                gsap.to($ball, {
-                  duration: 0.2,
-                  width: $ballWidth,
-                  height: $ballHeight,
-                  opacity: $ballOpacity,
-                });
-              });
-
-            // Ignore "data-cursor" on mousedown.
-            $(this)
-              .find("[data-cursor]")
-              .on("mousedown pointerdown", function () {
-                return false;
-              });
-
-            // Ignore "data-cursor" on hover.
-            $(this)
-              .find("[data-cursor]")
-              .on("mouseenter mouseover", function () {
-                $ball.find(".ball-drag").remove();
-                return false;
-              });
-          }
-        }
-      });
-
-      // Cursor close on hover.
-      $(".cursor-close").each(function () {
-        $(this).addClass("ball-close-enabled");
-        $(this)
-          .on("mouseenter", function () {
-            $ball.addClass("ball-close-enabled");
-            $ball.append('<div class="ball-close">Close</div>');
-            gsap.to($ball, {
-              duration: 0.3,
-              yPercent: -75,
-              width: 80,
-              height: 80,
-              opacity: 1,
-            });
-            gsap.from(".ball-close", { duration: 0.3, scale: 0, autoAlpha: 0 });
-          })
-          .on("mouseleave click", function () {
-            $ball.removeClass("ball-close-enabled");
-            gsap.to($ball, {
-              duration: 0.3,
-              yPercent: -50,
-              width: $ballWidth,
-              height: $ballHeight,
-              opacity: $ballOpacity,
-            });
-            $ball.find(".ball-close").remove();
-          });
-
-        // Hover on "cursor-close" inner elements.
-        $(
-          ".cursor-close a, .cursor-close button, .cursor-close .tt-btn, .cursor-close .hide-cursor"
-        )
-          .not(".not-hide-cursor") // omit from selection (class "not-hide-cursor" is for global use).
-          .on("mouseenter", function () {
-            $ball.removeClass("ball-close-enabled");
-          })
-          .on("mouseleave", function () {
-            $ball.addClass("ball-close-enabled");
-          });
-      });
-
-      // ================================================================
-      // Scroll between anchors
-      // ================================================================
-
-      $('a[href^="#"]')
-        .not('[href$="#"]') // omit from selection
-        .not('[href$="#0"]') // omit from selection
-        .on("click", function () {
-          var target = this.hash;
-
-          // If fixed header position enabled.
-          if ($("#tt-header").hasClass("tt-header-fixed")) {
-            var $offset = $("#tt-header").height();
-          } else {
-            var $offset = 0;
-          }
-
-          // You can use data attribute (for example: data-offset="100") to set top offset in HTML markup if needed.
-          if ($(this).data("offset") != undefined)
-            $offset = $(this).data("offset");
-
-          return false;
-        });
-
-      // Show/hide magic cursor
-      // =======================
-      // Hide on hover.
-      $(
-        "a, button, .tt-btn, .tt-form-control, .tt-form-radio, .tt-form-check, .hide-cursor"
-      ) // class "hide-cursor" is for global use.
-        .not(".not-hide-cursor") // omit from selection (class "not-hide-cursor" is for global use).
-        .not(".cursor-alter") // omit from selection
-        .not(".tt-main-menu-list > li > a") // omit from selection
-        .not(".tt-main-menu-list > li > .tt-submenu-trigger > a") // omit from selection
-        .on("mouseenter", function () {
-          gsap.to($ball, { duration: 0.3, scale: 0, opacity: 0 });
-        })
-        .on("mouseleave", function () {
-          gsap.to($ball, { duration: 0.3, scale: 1, opacity: $ballOpacity });
-        });
-
-      // Hide on click.
-      $("a")
-        .not('[target="_blank"]') // omit from selection.
-        .not('[href^="#"]') // omit from selection.
-        .not('[href^="mailto"]') // omit from selection.
-        .not('[href^="tel"]') // omit from selection.
-        .not(".lg-trigger") // omit from selection.
-        .not(".video-player") // omit from selection.
-        .not(".tt-btn-disabled") // omit from selection.
-        .on("click", function () {
-          gsap.to($ball, { duration: 0.3, scale: 1.3, autoAlpha: 0 });
-        });
-
-      // Show/hide on document leave/enter.
-      $(document)
-        .on("mouseleave", function () {
-          gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 0 });
-        })
-        .on("mouseenter", function () {
-          gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 1 });
-        });
-
-      // Show as the mouse moves.
-      $(document).mousemove(function () {
-        gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 1 });
-      });
-    }
-  }
-  // Magic cursor behavior
-		  // ======================
-	
-		  // Magnetic item hover.
-		  $(".magnetic-wrap")
-			.on("mouseenter mouseover", function (e) {
-			  $ball.addClass("magnetic-active");
-			  gsap.to($ball, { duration: 0.3, width: 70, height: 70, opacity: 1 });
-			  $active = true;
-			})
-			.on("mouseleave", function (e) {
-			  $ball.removeClass("magnetic-active");
-			  gsap.to($ball, {
-				duration: 0.3,
-				width: $ballWidth,
-				height: $ballHeight,
-				opacity: $ballOpacity,
-			  });
-			  gsap.to(this.querySelector(".magnetic-item"), {
-				duration: 0.3,
-				x: 0,
-				y: 0,
-				clearProps: "all",
-			  });
-			  $active = false;
-			});
-	
   //home1-banner-slider
   var swiper = new Swiper(".banner-img-slider", {
     slidesPerView: 1,
@@ -506,7 +126,6 @@
       prevEl: ".testimonial-slider-prev",
     },
   });
-  // home1 testimonial Slider end
   // Home1 Team Slider
   var swiper = new Swiper(".home1-team-slider", {
     slidesPerView: 1,
@@ -568,50 +187,8 @@
     $(window).scrollTo({ top: 0, behavior: "smooth" });
   }
   // smooth scroll end
-  // services Images
-  const serviceImgItem = document.querySelectorAll(
-    ".services-wrap .single-services "
-  );
-  console.log(serviceImgItem);
-  function followImageCursor(event, item, rotate = false) {
-    const contentBox = item.getBoundingClientRect();
-    const dx = event.clientX - contentBox.x;
-    const dy = event.clientY - contentBox.y;
-
-    const targetElement = item.children[2]; // Adjust index if needed
-    if (targetElement) {
-      targetElement.style.transform = rotate
-        ? `translate(${dx}px, ${dy}px) rotate(12deg)`
-        : `translate(${dx}px, ${dy}px)`;
-    }
-  }
-
-  serviceImgItem.forEach((item, i) => {
-    item.addEventListener("mousemove", (event) => {
-      setInterval(followImageCursor(event, item), 100);
-    });
-  });
-
-  const serviceImgItem2 = document.querySelectorAll(
-    ".services-wrap2 .single-services"
-  );
-  console.log(serviceImgItem2);
-  function followImageCursor(event, serviceImgItem2) {
-    const contentBox = serviceImgItem2.getBoundingClientRect();
-    const dx = event.clientX - contentBox.x;
-    const dy = event.clientY - contentBox.y;
-    serviceImgItem2.children[2].style.transform = `translate(${dx}px, ${dy}px) rotate(0deg)`;
-  }
-
-  serviceImgItem2.forEach((item, i) => {
-    item.addEventListener("mousemove", (event) => {
-      setInterval(followImageCursor(event, item), 100);
-    });
-  });
 
   // right sidebar start
-
-  // sidebar
   $(".right-sidebar-button").on("click", function () {
     $(".right-sidebar-menu").addClass("show-right-menu");
   });
@@ -626,83 +203,5 @@
   });
   //  right sidebar end
 
-  // Preloader
-  jQuery(window).on("load", function () {
-    $(".preloader").delay(1600).fadeOut("slow");
-  });
 
-  $(".sidebar-button").on("click", function () {
-    $(this).toggleClass("active");
-  });
-
-  document
-    .querySelector(".sidebar-button")
-    .addEventListener("click", () =>
-      document.querySelector(".main-menu").classList.toggle("show-menu")
-    );
-
-  $(".menu-close-btn").on("click", function () {
-    $(".main-menu").removeClass("show-menu");
-  });
-
-  // Title Animation
-  let splitTitleLines = gsap.utils.toArray(".text-animation");
-
-  splitTitleLines.forEach((splitTextLine) => {
-    const tl2 = gsap.timeline({
-      scrollTrigger: {
-        trigger: splitTextLine,
-        start: "top 90%",
-        end: "bottom 60%",
-        scrub: false,
-        markers: false,
-        toggleActions: "play none none none",
-      },
-    });
-
-    const itemSplitted = new SplitText(splitTextLine, { type: "words, lines" });
-    gsap.set(splitTextLine, { perspective: 400 });
-    itemSplitted.split({ type: "lines" });
-    tl2.from(itemSplitted.lines, {
-      duration: 1,
-      delay: 0.3,
-      opacity: 0,
-      rotationX: -80,
-      force3D: true,
-      transformOrigin: "top center -50",
-      stagger: 0.1,
-    });
-  });
-
-  // 27. Text Animation
-  let splitTextLines = gsap.utils.toArray(".text-animation p");
-
-  splitTextLines.forEach((splitTextLine) => {
-    const tl3 = gsap.timeline({
-      scrollTrigger: {
-        trigger: splitTextLine,
-        start: "top 90%",
-        duration: 2,
-        end: "bottom 60%",
-        scrub: false,
-        markers: false,
-        toggleActions: "play none none none",
-      },
-    });
-
-    const itemSplitted = new SplitText(splitTextLine, { type: "lines" });
-    gsap.set(splitTextLine, { perspective: 400 });
-    itemSplitted.split({ type: "lines" });
-    tl3.from(itemSplitted.lines, {
-      duration: 1,
-      delay: 0.5,
-      opacity: 0,
-      rotationX: -80,
-      force3D: true,
-      transformOrigin: "top center -50",
-      stagger: 0.1,
-    });
-  });
-  handleResize();
-  window.addEventLis;
 })(jQuery);
